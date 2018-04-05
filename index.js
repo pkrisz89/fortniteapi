673516e32860da5672e6d806f6a3bae428ec7f40
Kris Pinter <pinter.krisztian89@gmail.com>
2018-04-05 19:43:13 +0100

    add api request

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const http = require('http');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const apikey = require('./apikey');
 
 const app = express();
-app.server = http.createServer(app);
 
 app.use(bodyParser.json());
 app.use(
@@ -12,15 +12,28 @@ app.use(
   })
 );
 
-app.server.listen(process.env.PORT || 3000, () => {
-  console.log(`Started on port ${app.server.address().port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Started');
 });
 
 app.get('/', (req, res) => {
   res.json('hello');
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res.json(req.body.username);
-});
+app.post('/', (req, res) =>
+  getPlayerDetails(req.body.username).then(response => {
+    res.json(response);
+  })
+);
+
+function getPlayerDetails(nickname) {
+  const platform = 'pc' || 'xbl' || 'psn';
+  const url = `https://api.fortnitetracker.com/v1/profile/${platform}/${nickname}`;
+
+  return axios
+    .get(url, { headers: { 'TRN-Api-Key': apikey } })
+    .then(response => response.data)
+    .catch(error => {
+      console.log(error);
+    });
+}
