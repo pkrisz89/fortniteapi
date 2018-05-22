257a857848ba3b4844317b8770aeb43a73b695b3
Kris Pinter <pinter.krisztian89@gmail.com>
2018-05-22 19:53:38 +0100

    add logout

--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,61 @@
 const express = require('express');
 const router = express.Router();
 const getPlayerStats = require('../services/getPlayerStats');
-const {
-  comparePassword,
-  getUserByEmail,
-  doesUserExist
-} = require('../services/authenticationService');
-const {
-  findAndDelete,
-  getAllFriends,
-  addFriend,
-  addFriendAndRegister
-} = require('../services/friendsService');
-const { addUser } = require('../services/registrationService');
-const { User } = require('../models/User');
+const {comparePassword, getUserByEmail, doesUserExist} = require('../services/authenticationService');
+const {findAndDelete, getAllFriends, addFriend, addFriendAndRegister} = require('../services/friendsService');
+const {addUser} = require('../services/registrationService');
+const {User} = require('../models/User');
 const sessionChecker = require('../middlewares');
 
 router.get('/stats/:userid/', sessionChecker, (req, res, next) => {
-  User.findById(req.params.userid)
+  User
+    .findById(req.params.userid)
     .then(user => {
       getPlayerStats(user.username, user.platform).then(response => {
-        res.json({ response });
+        res.json({response});
       });
     })
     .catch(err => {
-      res.status(500).send('error occured!');
+      res
+        .status(500)
+        .send('error occured!');
     });
 });
 
 router.get('/mystats', (req, res, next) => {
-  User.findById(req.session.user.id)
+  User
+    .findById(req.session.user.id)
     .then(user => {
       getPlayerStats(user.username, user.platform).then(response => {
-        res.json({ response });
+        res.json({response});
       });
     })
     .catch(err => {
-      res.status(500).send('error occured!');
+      res
+        .status(500)
+        .send('error occured!');
     });
 });
 
 router.delete('/friend/:id', sessionChecker, (req, res, next) => {
-  findAndDelete(req)
-    .then(() => {
-      res.json({ msg: 'friend deleted' });
-    })
-    .catch(err => {
-      throw err;
-    });
+  findAndDelete(req).then(() => {
+    res.json({msg: 'friend deleted'});
+  }).catch(err => {
+    throw err;
+  });
 });
 
-router.get('/friends', sessionChecker, (req, res, next) =>
-  getAllFriends(req)
-    .then(friends => {
-      res.json({ friends });
-    })
-    .catch(err => {
-      throw err;
-    })
-);
+router.get('/friends', sessionChecker, (req, res, next) => getAllFriends(req).then(friends => {
+  res.json({friends});
+}).catch(err => {
+  throw err;
+}));
 
 router.post('/friends', sessionChecker, (req, res, done) => {
-  const { username, platform } = req.body;
+  const {username, platform} = req.body;
 
-  return User.findById(req.session.user.id)
+  return User
+    .findById(req.session.user.id)
     .then(self => {
       doesUserExist(username).then(friend => {
         if (friend) {
@@ -74,7 +66,7 @@ router.post('/friends', sessionChecker, (req, res, done) => {
       });
     })
     .catch(err => {
-      res.json({ msg: 'an error has occured' });
+      res.json({msg: 'an error has occured'});
       throw err;
     });
 });
@@ -83,20 +75,31 @@ router.post('/register', (req, res) => {
   addUser(req, res);
 });
 
+router.get('/logout', (req, res) => {
+  if (req.session.user && req.cookies.user_sid) {
+    res.clearCookie('user_sid');
+  }
+});
+
 router.post('/login', (req, res, next) => {
-  const { email, password } = req.body;
+  const {email, password} = req.body;
 
   getUserByEmail(email).then(user => {
     if (!user) {
-      res.status(500).send('Wrong username/password!');
+      res
+        .status(500)
+        .send('Wrong username/password!');
     } else {
       comparePassword(password, user.password, (err, isMatch) => {
-        if (err) throw err;
+        if (err)
+          throw err;
         if (isMatch) {
           req.session.user = user;
-          res.json({ success: true });
+          res.json({success: true});
         } else {
-          res.status(500).send('Wrong username/password!');
+          res
+            .status(500)
+            .send('Wrong username/password!');
         }
       });
     }
